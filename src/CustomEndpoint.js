54976612f971d0c5e627e1d301f499f6c4d34cd5
Simone Vellei <henomis@gmail.com>
2025-05-15 15:30:36 +0200

    Refactored custom connector for next generation Connectware

--- src/CustomEndpoint.js
+++ src/CustomEndpoint.js
@@ -5,15 +5,6 @@ class CustomEndpoint extends Endpoint {
     static getSchema() {
         return Object.assign(super.getSchema(), schema)
     }
-
-    constructor(options) {
-        super(options)
-        this._topic = options.address.address // Create the Endpoint MQTT topic
-    }
-
-    _getTopic() {
-        return this._topic
-    }
 }
 
 module.exports = CustomEndpoint
--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-const { VrpcAdapter } = require('vrpc')
 const Connection = require('./CustomConnection')
 const Endpoint = require('./CustomEndpoint')
-
-VrpcAdapter.register(Endpoint, { schema: Endpoint.getSchema() })
-VrpcAdapter.register(Connection, { schema: Connection.getSchema() })
